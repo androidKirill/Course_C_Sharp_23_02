750ab60cb7020c9be8d9fd2c8a2495cb17e65c41
mary <mm@mail.ru>
2023-03-02 09:45:44 +0300

    Task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// 3. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+void OneAndTwo(int num)
+{       
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine($"{num} -> yes");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> no");        
+    }    
+}
+
+int result = int.Parse(Console.ReadLine()!);
+OneAndTwo(result);
