0d007268bab006990a5fbf9f9d1aa2cf308e4613
mary <mm@mail.ru>
2023-03-03 19:26:50 +0300

    Some

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// double Distance(double x1, double y1, double x2, double y2)
+// {
+//     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+// }
+
+// Console.WriteLine(Distance(3, 6, 2, 1));
+// Console.WriteLine(Distance(7, -5, 1, -1));
+
+Console.WriteLine("Введите число: ");
+
+int number = int.Parse(Console.ReadLine()!);
+int result = GetThirdDigit(number);
+
+
+int GetThirdDigit(int number)
+{   
+    int i = 0;
+    while (number > 999)
+    {
+        number /= 10;
+        i = number % 10;
+    }
+    return i;
+}
+
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+    Console.WriteLine($"Третья цифра число {result}");
