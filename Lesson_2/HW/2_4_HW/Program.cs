71cac5147869eef10a2aac40e8713735bb2f9114
mary <mm@mail.ru>
2023-03-04 09:58:10 +0300

    Some changes

--- Lesson_2/HW/2_4_HW/Program.cs
+++ Lesson_2/HW/2_4_HW/Program.cs
@@ -3,11 +3,11 @@
 
 void Weekend(int num)
 {
-    string text = "no";
+    string text = "not quite a week";
 
     if (num == 7 || num == 6) text = "weekend";
     else if (num < 6 && num > 0) text = "workday";
-    else text = "not quite a week";
+    
     Console.WriteLine($"{num} -> {text}");
 }
 
