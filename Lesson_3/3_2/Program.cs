98673b34c98de55735ff7ac5cb04a39ee3232a74
mary <mm@mail.ru>
2023-03-06 09:53:20 +0300

    Some changes HW task_3_2

--- Lesson_3/3_2/Program.cs
+++ Lesson_3/3_2/Program.cs
@@ -7,5 +7,13 @@ double Distance(double x1, double y1, double x2, double y2)
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 }
 
-Console.WriteLine(Distance(3, 6, 2, 1));
-Console.WriteLine(Distance(7, -5, 1, -1));
+double x_1 = double.Parse(Console.ReadLine()!);
+double y_1 = double.Parse(Console.ReadLine()!);
+double z_1 = double.Parse(Console.ReadLine()!);
+double x_2 = double.Parse(Console.ReadLine()!);
+double y_2 = double.Parse(Console.ReadLine()!);
+double z_2 = double.Parse(Console.ReadLine()!);
+
+// 3, 6, 8, 2, 1, -7
+// 7, -5, 0, 1, -1, 9
+Console.WriteLine(Line3D(x_1, y_1, z_1, x_2, y_2, z_2));
