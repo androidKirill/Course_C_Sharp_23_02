194c14919f339e06235f849f401a1ac6337029ff
mary <mm@mail.ru>
2023-03-09 08:42:12 +0300

    Changes to previous tasks

--- Lesson_3/3_2/Program.cs
+++ Lesson_3/3_2/Program.cs
@@ -2,7 +2,7 @@
 //    двух точек и находит расстояние между ними в 2D пространстве.
 //    https://ru.onlinemschool.com/math/library/analytic_geometry/point_point_length/
 
-double Distance(double x1, double y1, double x2, double y2)
+double Line3D(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 }
